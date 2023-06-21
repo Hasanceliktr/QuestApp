c6a3eba9efc897a879c1ee0aa12eadaa83aa1a26
Hasan Çelik <hasancelikdeveloper@gmail.com>
2023-06-22 02:10:57 +0300

    Test commit and push

--- src/main/java/com/hasanceliktr/questapp/QuestAppApplication.java
+++ src/main/java/com/hasanceliktr/questapp/QuestAppApplication.java
@@ -8,6 +8,7 @@ public class QuestAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuestAppApplication.class, args);
+        System.out.println("Test commit");
     }
 
 }
